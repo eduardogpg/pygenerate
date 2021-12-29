23b2973bbef592d0ba49b2eca1eb8e90f198b4b1
Eduardo <eduardo78d@gmail.com>
2021-12-29 10:13:41 -0600

    setups files

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name = 'pybose',
+    packages = ['app'],
+    version = '0.1',
+    license='MIT',
+    description = 'Python generator project',
+    author = 'Eduardo Ismael García Pérez',
+    author_email = 'eduardo78d@gmail.com',
+    url = 'https://github.com/eduardogpg/pybose',
+    download_url = 'https://github.com/eduardogpg/pybose/archive/refs/tags/0.1.tar.gz',
+    keywords = ['Python', 'Generator', 'Project'],
+    install_requires=[
+        'click'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Developers',      # Define that your audience are developers
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
