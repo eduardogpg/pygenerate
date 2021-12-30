59b00df0a0608605e911013db18c66b1712a496c
eduardogpg <eduardo78d@gmail.com>
2021-12-29 19:46:24 -0600

    setup

--- setup.py
+++ setup.py
@@ -1,22 +1,33 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Python generator project'
 
 setup(
-    name = 'pylogi',
-    packages = ['pylogi'],
-    version = '0.4',
+    name = 'Package Name',
+    packages = ['Package Name'],
+    version = VERSION,
     license='MIT',
-    description = 'Python generator project',
+    description = DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
     author = 'Eduardo Ismael García Pérez',
     author_email = 'eduardo78d@gmail.com',
-    url = 'https://github.com/eduardogpg/pybose',
-    keywords = ['Python', 'Generator', 'Project'],
-    install_requires=[
-        'click'
+    url = 'https://github.com/eduardogpg/pygenerate',
+    keywords = ['Python Generate', 'Generate', 'Project'],
+    install_requires=[ 
+        'click',
     ],
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
     ],
 )
