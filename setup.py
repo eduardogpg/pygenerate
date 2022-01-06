d16b8590a2b127c61f1b0139f1f72771fce7946d
eduardogpg <eduardo78d@gmail.com>
2022-01-05 20:11:01 -0600

    0.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
-VERSION = '0.1.2'
+VERSION = '0.1.3'
 DESCRIPTION = 'Python generator project'
 
 setup(
